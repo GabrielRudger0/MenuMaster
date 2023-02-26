130d25b5f429698e6e5773b2b8c71ddeb65445ec
Gabs Rudger <gabrielrudger2003@gmail.com>
2023-02-26 01:12:37 -0300

    Registro de cardapio atualizado com mensagem pós registro; Implementado inserção de bebidas no cardapio separado dos pratos; Implementado mais infos para todos os itens; Implementado cardápio dinâmico com inserção de novos pratos e bebidas; Implementado função de fazerPedido; Implementado registro de pedido no banco de dados;

--- Restaurante20/src/restaurantepkg/TelaCardapio.java
+++ Restaurante20/src/restaurantepkg/TelaCardapio.java
@@ -72,6 +72,7 @@ public class TelaCardapio {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cardapioDAO.save(cardapioObject);
+                JOptionPane.showInputDialog(null,"Item registrado no cardápio","Cardapio",JOptionPane.INFORMATION_MESSAGE);
                 System.exit(0);
 
             }
--- Restaurante20/src/restaurantepkg/TelaFazerPedido.java
+++ Restaurante20/src/restaurantepkg/TelaFazerPedido.java
@@ -1,6 +1,7 @@
 package restaurantepkg;
 
 import restauranteDAO.CardapioDAO;
+import restauranteDAO.PedidoDAO;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +10,12 @@ import java.awt.event.ActionListener;
 public class TelaFazerPedido {
 
     public CardapioDAO cardapioDAO = new CardapioDAO();
+    public PedidoDAO pedidoDAO = new PedidoDAO();
+    public PedidoPkg pedido = new PedidoPkg();
+
+    public int[] sequencialIndexes = {-1,-1,-1,-1,-1};
+
+    public float precoPratoParaFolhaPedido;
     private JPanel FazerPedido;
     private JLabel MenuMaster;
     private JLabel nomePrato1;
@@ -38,50 +45,113 @@ public class TelaFazerPedido {
     private JLabel valorPrato5;
     private JButton fazerPedido5;
     private JButton maisInfos5;
+    private JLabel valorBebida1;
+    private JButton fazerPedidoBebida1;
+    private JLabel nomeBebida2;
+    private JLabel descricaoBebida2;
+    private JLabel valorBebida2;
+    private JLabel descricaoBebida3;
+    private JLabel nomeBebida3;
+    private JLabel valorBebida3;
+    private JButton fazerPedidoBebida2;
+    private JButton fazerPedidoBebida3;
+    private JButton fazerPedidoBebida4;
+    private JButton fazerPedidoBebida5;
+    private JLabel descricaoBebida4;
+    private JLabel descricaoBebida5;
+    private JLabel valorBebida4;
+    private JLabel valorBebida5;
+    private JLabel nomeBebida4;
+    private JLabel nomeBebida5;
+    private JButton cofirmarCompraButton;
+    private JButton botaoCancelar;
 
     public TelaFazerPedido() {
 
-        //INSERÇÃO DE PRATOS
+        //INSERÇÃO DE PRATOS E BEBIDAS
 
         //Prato 1
-        if(cardapioDAO.getCardapio().size() >= 1) {
-            this.inserirNoCardapio(nomePrato1, DescricaoPrato1, valorPrato1, fazerPedidoButton, maisInformacoes1Button, 0);
+        if (cardapioDAO.getCardapio().size() >= 1) {
+
+            if (this.eBebida(0)) {
+                fazerPedidoButton.setVisible(false);
+                maisInformacoes1Button.setVisible(false);
+                this.inserirBebida(0, nomeBebida1, descricaoBebida1, valorBebida1, fazerPedidoBebida1);
+            } else {
+                fazerPedidoBebida1.setVisible(false);
+                this.inserirNoCardapio(nomePrato1, DescricaoPrato1, valorPrato1, fazerPedidoButton, maisInformacoes1Button, 0);
+            }
         } else {
+            fazerPedidoBebida1.setVisible(false);
             fazerPedidoButton.setVisible(false);
             maisInformacoes1Button.setVisible(false);
         }
 
         //Prato 2
-        if(cardapioDAO.getCardapio().size() >= 2) {
-            this.inserirNoCardapio(nomePrato2, DescricaoPrato2, valorPrato2, fazerPedido2, maisInfos2, 1);
+        if (cardapioDAO.getCardapio().size() >= 2) {
+            if (this.eBebida(1)) {
+                fazerPedido2.setVisible(false);
+                maisInfos2.setVisible(false);
+                this.inserirBebida(1, nomeBebida2, descricaoBebida2, valorBebida2, fazerPedidoBebida2);
+            } else {
+                fazerPedidoBebida2.setVisible(false);
+                this.inserirNoCardapio(nomePrato2, DescricaoPrato2, valorPrato2, fazerPedido2, maisInfos2, 1);
+            }
+
         } else {
+            fazerPedidoBebida2.setVisible(false);
             fazerPedido2.setVisible(false);
             maisInfos2.setVisible(false);
         }
 
         //Prato 3
         if (cardapioDAO.getCardapio().size() >= 3) {
-            this.inserirNoCardapio(nomePrato3, DescricaoPrato3, valorPrato3, fazerPedido3, maisInfos3, 2);
+            if (this.eBebida(2)) {
+                fazerPedido3.setVisible(false);
+                maisInfos3.setVisible(false);
+                this.inserirBebida(2, nomeBebida3, descricaoBebida3, valorBebida3, fazerPedidoBebida3);
+            } else {
+                fazerPedidoBebida3.setVisible(false);
+                this.inserirNoCardapio(nomePrato3, DescricaoPrato3, valorPrato3, fazerPedido3, maisInfos3, 2);
+            }
+
         } else {
+            fazerPedidoBebida3.setVisible(false);
             fazerPedido3.setVisible(false);
             maisInfos3.setVisible(false);
         }
         //Prato 4
         if (cardapioDAO.getCardapio().size() >= 4) {
-            this.inserirNoCardapio(nomePrato4, DescricaoPrato4, valorPrato4, fazerPedido4, maisInfos4, 3);
+            if (this.eBebida(3)) {
+                fazerPedido4.setVisible(false);
+                maisInfos4.setVisible(false);
+                this.inserirBebida(3, nomeBebida4, descricaoBebida4, valorBebida4, fazerPedidoBebida4);
+            } else {
+                fazerPedidoBebida4.setVisible(false);
+                this.inserirNoCardapio(nomePrato4, DescricaoPrato4, valorPrato4, fazerPedido4, maisInfos4, 3);
+            }
+
         } else {
+            fazerPedidoBebida4.setVisible(false);
             fazerPedido4.setVisible(false);
             maisInfos4.setVisible(false);
         }
 
         if (cardapioDAO.getCardapio().size() >= 5) {
-            this.inserirNoCardapio(nomePrato5, DescricaoPrato5, valorPrato5, fazerPedido5, maisInfos5, 4);
+            if (this.eBebida(4)) {
+                fazerPedido5.setVisible(false);
+                maisInfos5.setVisible(false);
+                this.inserirBebida(4, nomeBebida5, descricaoBebida5, valorBebida5, fazerPedidoBebida5);
+            } else {
+                fazerPedidoBebida5.setVisible(false);
+                this.inserirNoCardapio(nomePrato5, DescricaoPrato5, valorPrato5, fazerPedido5, maisInfos5, 4);
+            }
+
         } else {
+            fazerPedidoBebida5.setVisible(false);
             fazerPedido5.setVisible(false);
             maisInfos5.setVisible(false);
         }
-
-
 
 
         maisInformacoes1Button.addActionListener(new ActionListener() {
@@ -99,6 +169,119 @@ public class TelaFazerPedido {
                 telaInformacoes.iniciarTelaInformacoesPedido(1);
             }
         });
+        maisInfos3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TelaInformacoesPrato telaInformacoes = new TelaInformacoesPrato(2);
+                telaInformacoes.iniciarTelaInformacoesPedido(2);
+            }
+        });
+        maisInfos4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TelaInformacoesPrato telaInformacoes = new TelaInformacoesPrato(3);
+                telaInformacoes.iniciarTelaInformacoesPedido(3);
+            }
+        });
+        maisInfos5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TelaInformacoesPrato telaInformacoes = new TelaInformacoesPrato(4);
+                telaInformacoes.iniciarTelaInformacoesPedido(4);
+            }
+        });
+
+        fazerPedidoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registrarPedido(0);
+
+            }
+        });
+        fazerPedido2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registrarPedido(1);
+            }
+        });
+        fazerPedido3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registrarPedido(2);
+            }
+        });
+        fazerPedido4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registrarPedido(3);
+            }
+        });
+        fazerPedido5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                registrarPedido(4);
+            }
+        });
+        fazerPedidoBebida1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = sequencialIndexes[0];
+                registrarPedido(sequencialIndexes[index]);
+            }
+        });
+        fazerPedidoBebida2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = sequencialIndexes[1];
+                registrarPedido(index);
+            }
+        });
+        fazerPedidoBebida3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (sequencialIndexes[2] != -1) {
+                    registrarPedido(sequencialIndexes[2]);
+                }
+            }
+        });
+        fazerPedidoBebida4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (sequencialIndexes[3] != -1){
+                    registrarPedido(sequencialIndexes[3]);
+                }
+
+            }
+        });
+        fazerPedidoBebida5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (sequencialIndexes[4] != -1) {
+                    registrarPedido(sequencialIndexes[4]);
+                }
+            }
+        });
+        cofirmarCompraButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object[] opcaoConfirma = {"Confirmar", "Sair"};
+                int opcaoSelecionada = JOptionPane.showOptionDialog(null, "Prato/Bebida: " + pedido.getItenspedidos() +
+                                "\nPreço: R$ " + precoPratoParaFolhaPedido +
+                                "\nQuantidade: " + pedido.getQuantidade() +
+                                "\nObservação: " + pedido.getObservacao(), "Obervação", JOptionPane.OK_CANCEL_OPTION,
+                                JOptionPane.QUESTION_MESSAGE, null, opcaoConfirma, opcaoConfirma[0]);
+                if (opcaoSelecionada == 0) {
+                    JOptionPane.showMessageDialog(null, "Seu pedido chegará em instantes, Obrigado!");
+                    pedidoDAO.save(pedido);
+                }
+            }
+        });
+        botaoCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     public void iniciarTelaFazerPedido() {
@@ -114,19 +297,89 @@ public class TelaFazerPedido {
     public void inserirNoCardapio(JLabel nomePrato, JLabel DescricaoPrato, JLabel valorPrato,
                                   JButton fazerPedido, JButton maisInfos, int indexArray) {
 
-        fazerPedido.setVisible(true);
-        maisInfos.setVisible(true);
+        this.organizaSequenciaDeIndex(indexArray);
+
         Cardapio prato = new Cardapio();
         prato = cardapioDAO.getCardapio().get(indexArray);
-
         String nome1 = prato.getNome_prato();
         String descricao1 = prato.getIngredientes();
         String preco1 = "R$ " + (prato.getPreco());
 
+        fazerPedido.setVisible(true);
+        maisInfos.setVisible(true);
         nomePrato.setText(nome1);
         DescricaoPrato.setText(descricao1);
         valorPrato.setText(preco1);
+
     }
 
+    public void inserirBebida(int indexArray, JLabel nomeBebida, JLabel descricaoBebida, JLabel valorBebida,
+                              JButton fazerPedidoBebida) {
 
+        this.organizaSequenciaDeIndex(indexArray);
+
+        Cardapio bebida = new Cardapio();
+        bebida = cardapioDAO.getCardapio().get(indexArray);
+
+        String nome1 = bebida.getNome_prato();
+        String descricao1 = bebida.getIngredientes();
+        String preco1 = "R$ " + (bebida.getPreco());
+
+        fazerPedidoBebida.setVisible(true);
+        nomeBebida.setText(nome1);
+        descricaoBebida.setText(descricao1);
+        valorBebida.setText(preco1);
+
+    }
+
+    public void registrarPedido(int indexArray) {
+        Cardapio prato = new Cardapio();
+
+        prato = cardapioDAO.getCardapio().get(indexArray);
+        int qtd = 1;
+        Object[] opcaoSimNao = {"Sim", "Não"};
+        String obs = "Nenhuma Observação";
+        int opcaoSelecionada = 0;
+        precoPratoParaFolhaPedido = prato.getPreco();
+
+        opcaoSelecionada = JOptionPane.showOptionDialog(null, "Deseja fazer uma observação?", "Obervação",
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcaoSimNao, opcaoSimNao[1]);
+
+        String pratoPedido = prato.getNome_prato();
+        pedido.setItenspedidos(pratoPedido);
+        pedido.setQuantidade("" + qtd);
+        if (opcaoSelecionada == 0) {
+            obs = JOptionPane.showInputDialog(null, "Observação:", "Observação", JOptionPane.INFORMATION_MESSAGE);
+            pedido.setObservacao(obs);
+
+        }
+    }
+
+        public boolean eBebida ( int indexArray){
+            boolean eBebidaSN = false;
+            Cardapio bebida = new Cardapio();
+            bebida = cardapioDAO.getCardapio().get(indexArray);
+
+            if (bebida.getCategoria().equals("Bebidas Alcoólicas") || bebida.getCategoria().equals("Refrescos e Sucos")) {
+                eBebidaSN = true;
+            }
+            return eBebidaSN;
+
+
+    }
+    public void organizaSequenciaDeIndex(int indexArray) {
+        if (sequencialIndexes[0] == -1) {
+            sequencialIndexes[0] = indexArray;
+
+        } else if(sequencialIndexes[1] == -1){
+            sequencialIndexes[1] = indexArray;
+
+        } else if(sequencialIndexes[2] == -1) {
+            sequencialIndexes[2] = indexArray;
+        } else if(sequencialIndexes[3] == -1) {
+            sequencialIndexes[3] = indexArray;
+        } else if(sequencialIndexes[4] == -1) {
+            sequencialIndexes[4] = indexArray;
+        }
+    }
 }
