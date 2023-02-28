023c91fe445268fc2b9b3af7d6876d9510696005
Gabs Rudger <gabrielrudger2003@gmail.com>
2023-02-28 17:10:15 -0300

    Implementado "TelaContabilMenu" BETA

---
+++ Restaurante20/src/restaurantepkg/TelaContabilMenu.java
@@ -0,0 +1,37 @@
+package restaurantepkg;
+
+import RestauranteAplication.Main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaContabilMenu {
+    private JButton relatorioGeralButton;
+    private JButton detalhesDeReceitasButton;
+    private JButton detalhesDeDespesasButton;
+    public JPanel TelaContabilidadeMenu;
+
+    public TelaContabilMenu() {
+    relatorioGeralButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+    detalhesDeReceitasButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+    detalhesDeDespesasButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+        }
+    });
+}
+
+}
