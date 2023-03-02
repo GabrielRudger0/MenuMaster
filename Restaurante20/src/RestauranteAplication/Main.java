0eda709c036da7c174816ba4bcfacb4438bd3b64
MuriloCordova <muriloocordova@gmail.com>
2023-03-01 21:00:21 -0300

    MAIOR COMMIT

--- Restaurante20/src/RestauranteAplication/Main.java
+++ Restaurante20/src/RestauranteAplication/Main.java
@@ -1,10 +1,7 @@
 package RestauranteAplication;
 
-import restaurante.factory.ViaCEP;
-import restauranteDAO.ClienteDAO;
 import restaurantepkg.*;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class Main {
@@ -12,15 +9,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ExecutaTelas executaTelas = new ExecutaTelas();
 
+        System.out.println(br.com.correios.ClienteWs.getEnderecoPorCep("89050570"));
+       // System.out.println(br.com.correios.ClienteWs.getMapPorCep("70002900"));
 
-        ViaCEP viacep = new ViaCEP("89050570");
-        viacep.buscar();
-        System.out.println(viacep.getLogradouro()); // imprime "Praça da Sé"
-        System.out.println(viacep.getBairro()); // imprime "Sé"
-        System.out.println(viacep.getLocalidade()); // imprime "São Paulo"
-        System.out.println(viacep.getUf()); // imprime "SP"
 
         //executaTelas.iniciarTelaLogin();
-        executaTelas.iniciarTelaAdmin();
+       executaTelas.iniciarTelaAdmin();
     }
 }
