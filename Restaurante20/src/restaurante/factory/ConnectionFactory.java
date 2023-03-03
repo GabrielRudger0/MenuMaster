8a3b6a5be07be790d7311f39f3cee263f54ee93e
muriloocordova <muriloocordova@gmail.com>
2023-03-03 18:01:36 -0300

    FUNCIONANDO FUNÇÃO DA API POR CEP

--- Restaurante20/src/restaurante/factory/ConnectionFactory.java
+++ Restaurante20/src/restaurante/factory/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class    ConnectionFactory {
 
     //nome do usuario
     private static final String USERNAME = "root";
