cf9b364204c94a397bb39a4359b7b8834d2cd627
muriloocordova <muriloocordova@gmail.com>
2023-03-03 19:41:57 -0300

    Implementando nova API do Correios para Registrar Clientes

--- Restaurante20/src/restaurantepkg/TelaRegistro.java
+++ Restaurante20/src/restaurantepkg/TelaRegistro.java
@@ -1,6 +1,6 @@
 package restaurantepkg;
 
-import RestauranteAplication.Main;
+import br.com.correios.Endereco;
 import restauranteDAO.ClienteDAO;
 
 import javax.swing.*;
@@ -18,69 +18,117 @@ public class TelaRegistro {
     private JButton voltarButton;
     private JTextField inserirTelefone;
     private JTextField insiraSenha;
+    private JTextField InserirRua;
+    private JTextField InserirNumero;
+    private JTextField InserirEstado;
+    private JTextField InserirCidade;
+    private JTextField textField1;
+    private JTextField inserirBairro;
 
-public TelaRegistro() {
+    public TelaRegistro() {
 
-    ClienteDAO clienteDAO = new ClienteDAO();
-    Clientes cliente = new Clientes();
 
-    registrarButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (registroECorreto(cliente) != false) {
-                clienteDAO.save(cliente);
-                JOptionPane.showMessageDialog(null, "Registro Concluído!", "Registro", JOptionPane.INFORMATION_MESSAGE);
+        ClienteDAO clienteDAO = new ClienteDAO();
+        Clientes cliente = new Clientes();
+
+        registrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (registroECorreto(cliente) != false) {
+                    clienteDAO.save(cliente);
+                    JOptionPane.showMessageDialog(null, "Registro Concluído!", "Registro", JOptionPane.INFORMATION_MESSAGE);
+
+                }
+            }
+        });
+
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ExecutaTelas executaTelas = new ExecutaTelas();
+                ExecutaTelas.frameTelaRegistro.dispose();
+                executaTelas.iniciarTelaAdmin();
+            }
+        });
+
+        inserirNome.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                cliente.setNomeCliente(inserirNome.getText());
+                System.out.println(inserirNome.getText());
+            }
+        });
+        inserirCep.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                cliente.setCepCliente(inserirCep.getText());
+            }
+        });
+        inserirEmail.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                cliente.setEmailCliente(inserirEmail.getText());
+            }
+        });
+        inserirTelefone.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                cliente.setTelefoneCliente(inserirTelefone.getText());
+            }
+        });
+        insiraSenha.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                cliente.setSenha_cliente(insiraSenha.getText());
+            }
+        });
+        InserirRua.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+
+                super.focusGained(e);
+                Endereco enderecoapi = br.com.correios.ClienteWs.getEnderecoPorCep(inserirCep.getText());
+                InserirRua.setText(enderecoapi.getLogradouro());
 
             }
-        }
-    });
+        });
+        InserirEstado.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                Endereco enderecoapi = br.com.correios.ClienteWs.getEnderecoPorCep(inserirCep.getText());
+                InserirEstado.setText(enderecoapi.getUf());
 
-    voltarButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            ExecutaTelas executaTelas = new ExecutaTelas();
-            ExecutaTelas.frameTelaRegistro.dispose();
-            executaTelas.iniciarTelaAdmin();
-        }
-    });
+            }
+        });
 
-    inserirNome.addFocusListener(new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            super.focusLost(e);
-            cliente.setNomeCliente(inserirNome.getText());
-            System.out.println(inserirNome.getText());
-        }
-    });
-    inserirCep.addFocusListener(new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            super.focusLost(e);
-            cliente.setCepCliente(inserirCep.getText());
-        }
-    });
-    inserirEmail.addFocusListener(new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            super.focusLost(e);
-            cliente.setEmailCliente(inserirEmail.getText());
-        }
-    });
-    inserirTelefone.addFocusListener(new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            super.focusLost(e);
-            cliente.setTelefoneCliente(inserirTelefone.getText());
-        }
-    });
-    insiraSenha.addFocusListener(new FocusAdapter() {
-        @Override
-        public void focusLost(FocusEvent e) {
-            super.focusLost(e);
-            cliente.setSenha_cliente(insiraSenha.getText());
-        }
-    });
-}
+
+        InserirCidade.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+
+                super.focusGained(e);
+                Endereco enderecoapi = br.com.correios.ClienteWs.getEnderecoPorCep(inserirCep.getText());
+                InserirCidade.setText(enderecoapi.getCidade());
+            }
+        });
+
+
+        inserirBairro.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                Endereco enderecoapi = br.com.correios.ClienteWs.getEnderecoPorCep(inserirCep.getText());
+                inserirBairro.setText(enderecoapi.getBairro());
+
+            }
+        });
+    }
     public boolean registroECorreto(Clientes cliente) {
 
         String textoTela = "Informações não preenchidas!\n";
