83889b1da0d5ab69a696c7e4a3f55a856b8b0b15
Gabs Rudger <gabrielrudger2003@gmail.com>
2023-03-05 18:22:11 -0300

    Feature: Tela de Avaliações v1 by Ramon

--- Restaurante20/src/RestauranteAplication/Main.java
+++ Restaurante20/src/RestauranteAplication/Main.java
@@ -21,6 +21,7 @@ public class Main {
         System.out.println("AHHFADHFADHFIADHFIPADHFIOPADHFIOPDHOPFAD");
 
         executaTelas.iniciarTelaLogin();
+        //executaTelas.iniciarTelaAvaliacoes();
         //executaTelas.iniciarTelaFazerPedido();
         //executaTelas.iniciarTelaAdmin();
         //executaTelas.iniciarTelaRegistro();
